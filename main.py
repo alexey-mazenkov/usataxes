c30c343dc9c5d188ff4e0f1799b8f47009b791d9
KKravtsov14 <kostya.kravtsov.2001@mail.ru>
2019-10-11 15:04:49 +0700

    corrected input, entered numbers in the first function

--- main.py
+++ main.py
@@ -30,11 +30,11 @@ def formula(
     # На следующую ступень ставки налогообложения
 
     rate_1 = first
-    rate_2 = second
-    rate_3 = third
-    rate_4 = fourth
-    rate_5 = fifth
-    rate_6 = sixth
+    rate_2 = second - first
+    rate_3 = third - second
+    rate_4 = fourth - third
+    rate_5 = fifth - fourth
+    rate_6 = sixth - fifth
 
     # Заведем переменные для налоговых сборов с каждой ступени налога
 
@@ -44,12 +44,12 @@ def formula(
     lim_4 = 0.28 * rate_4
     lim_5 = 0.33 * rate_5
     lim_6 = 0.35 * rate_6
-    if income <= 457600:
-        if income <= 405100:
-            if income <= 226850:
-                if income <= 148850:
-                    if income <= 73800:
-                        if income <= 18150:
+    if income <= sixth:
+        if income <= fifth:
+            if income <= fourth:
+                if income <= third:
+                    if income <= second:
+                        if income <= first:
                             price = 0.1 * income
                         else:
                             price = lim_1 + 0.15 * (income - rate_1)
@@ -65,13 +65,13 @@ def formula(
         price = lim_1 + lim_2 + lim_3 + lim_4 + lim_5 + lim_6 + 0.396 * (income - rate_6)
 
 if subject_type == 'one subject':
-    formula()   # Для одного субъекта
+    formula(9075, 36900, 89350, 186350, 405100, 406750)   # Для одного субъекта
 
 if subject_type == 'married couple':
-    formula(18150, 55650, 73050, 78000, 178250, 52500) # Для супружеской пары
+    formula(18150, 73800, 148850, 226850, 405100, 457600) # Для супружеской пары
 
 if subject_type == 'single parent':
-formula()   # Для родителя-одиночки
+    formula()   # Для родителя-одиночки
 
 
 def main():
